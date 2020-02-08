c6636007cb012c3e9d4877a96c4e49a14c422610
Gorodetskaya-Mariia <1907mss@gmail.com>
2020-02-08 22:16:11 +0300

    Loot component and tests have been added

--- lootcheck/src/components/App.js
+++ lootcheck/src/components/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import Wallet from './Wallet';
+import Wallet from "./Wallet";
+import Loot from "./Loot";
 
 configure({ adapter: new Adapter() });
 
 class App extends React.Component {
-	render(){
-		return (
-			<div>
-				<h2>Loot Check</h2>
-				<hr />
-				<Wallet />
-			</div>
-		);	
-	}
+  render() {
+    return (
+      <div>
+        <h2>Loot Check</h2>
+        <hr />
+        <Wallet />
+        <hr />
+        <Loot />
+      </div>
+    );
+  }
 }
 
 export default App;
